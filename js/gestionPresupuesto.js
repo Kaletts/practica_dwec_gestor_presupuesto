628e89a648547a81740070cd36cdf38aed51e5aa
Kaled Jaluf <jalufkaled@gmail.com>
2024-10-19 18:13:18 +0200

    Nuevos metodos y verificaciones sobre fecha y etiquetas

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -6,48 +6,100 @@ let gastos = [];
 let idGasto = 0;
 
 function actualizarPresupuesto(importe) {
-    if(importe < 0 || isNaN(importe) == true) {
+    if (importe < 0 || isNaN(importe) == true) {
         console.log("El valor ingresado no es válido")
         return -1;
     }
     presupuesto = importe;
 
     return presupuesto;
-    }
-    
+}
+
 
 function mostrarPresupuesto() {
     return `Tu presupuesto actual es de ${presupuesto} €`;
 }
 
-function CrearGasto(descripcionGasto, valorGasto) {
-    if(valorGasto < 0 || isNaN(valorGasto) == true) {
+function CrearGasto(descripcion, valor, fecha, ...etiquetas) {
+    if (valor < 0 || isNaN(valor)) {
         this.valor = 0;
     } else {
-        this.valor = valorGasto;
+        this.valor = valor;
     }
-    this.descripcion = descripcionGasto;
+    this.descripcion = descripcion;
+    
+    //Para la verificación de la fecha lei en MDM web docs que si el valor de Date.parse falla se devuelve NaN
+    if(!fecha || isNaN(Date.parse(fecha))) {
+        this.fecha = Date.now();
+    } else {
+        this.fecha = fecha;
+    }
+    //Verifica que la etiqueta no este vacia
+    if(!etiquetas || etiquetas.length === 0) {
+        this.etiquetas = [];
+    }else {
+        this.etiquetas = etiquetas;
+    }
 
-    this.mostrarGasto = function() {
+    this.mostrarGasto = function () {
         return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`
     }
-    this.actualizarDescripcion = function(nuevaDescripcion){
+    this.mostrarGastoCompleto = function() {
+        let mensaje = `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`;
+        let fecha = `Fecha: ${this.fecha.toLocaleString()}`;
+        let etiquetas = `Etiquetas: ${this.etiquetas.forEach((etiqueta) => "\n - " + etiqueta.toString())}`;
+        mensaje.concat(fecha, etiquetas);
+        return mensaje;
+    }
+    this.actualizarDescripcion = function (nuevaDescripcion) {
         this.descripcion = nuevaDescripcion;
     }
-    this.actualizarValor = function(nuevoValor) {
-        if(nuevoValor < 0 || isNaN(nuevoValor) == true) {
+    this.actualizarValor = function (nuevoValor) {
+        if (nuevoValor < 0 || isNaN(nuevoValor)) {
             return this.valor;
         } else {
             this.valor = nuevoValor;
         }
     }
-   
+}
+
+function listarGastos() {
+    return gastos;
+}
+
+function anyadirGasto(gasto) {
+    gasto.id = idGasto;
+    ++idGasto;
+    gastos.push(gasto);
+}
+
+function borrarGasto(gasto) {
+    let index = gastos.findIndex(gasto.id);
+
+    if(index > -1) {
+        gastos.splice(index);
+    }
+}
+
+function calcularTotalGastos(){
+    let total = 0;
+
+    gastos.forEach(gasto => {
+        total += gasto.valor;
+    });
+    return total;
+}
+
+function calcularBalance() {
+    let totalGastos = calcularTotalGastos();
+    let balance = presupuesto - totalGastos;
+    return balance;
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
-export   {
+export {
     mostrarPresupuesto,
     actualizarPresupuesto,
     CrearGasto,
